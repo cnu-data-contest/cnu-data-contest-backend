730d1cea44586a82acffbf42eff04c5b0c465654
Kimdaemin <56399681+daemin-kim@users.noreply.github.com>
2023-05-16 11:10:03 +0900

    add MajorBaseUrl

---
+++ src/main/java/com/cnu/contestarchive/Domain/MajorBaseUrl.java
@@ -0,0 +1,218 @@
+package com.cnu.contestarchive.Domain;
+
+import lombok.Getter;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Getter
+public class MajorBaseUrl {
+
+    Map<String, Object> majorBaseUrl = new HashMap<>();
+
+    public MajorBaseUrl() {
+        majorBaseUrl.put("정보화본부", "https://cic.cnu.ac.kr");
+
+        majorBaseUrl.put("전기전자통신공학교육과", "https://eedu.cnu.ac.kr");
+
+        majorBaseUrl.put("건설공학교육과", "https://conedu.cnu.ac.kr");
+
+        majorBaseUrl.put("국어국문학과", "https://korean.cnu.ac.kr");
+
+        majorBaseUrl.put("사범대학", "https://edu.cnu.ac.kr");
+
+        majorBaseUrl.put("유기재료공학과", "https://omeng.cnu.ac.kr");
+
+        majorBaseUrl.put("일어일문학과", "https://cnujapan.cnu.ac.kr");
+
+        majorBaseUrl.put("체육교육과", "https://sportsedu.cnu.ac.kr");
+
+        majorBaseUrl.put("선박해양공학과", "https://ship.cnu.ac.kr");
+
+        majorBaseUrl.put("기계공학교육과", "https://mmee.cnu.ac.kr");
+
+        majorBaseUrl.put("예술대학", "https://art.cnu.ac.kr");
+
+        majorBaseUrl.put("항공우주공학과", "https://aero.cnu.ac.kr");
+
+        majorBaseUrl.put("컴퓨터융합학부", "https://computer.cnu.ac.kr");
+
+        majorBaseUrl.put("건축공학과", "https://archi-eng.cnu.ac.kr");
+
+        majorBaseUrl.put("수의과대학", "https://vetmed.cnu.ac.kr");
+
+        majorBaseUrl.put("회화과", "https://fineart.cnu.ac.kr");
+
+        majorBaseUrl.put("관현악과", "https://orch.cnu.ac.kr");
+
+        majorBaseUrl.put("국가안보융합학부", "https://soins.cnu.ac.kr");
+
+        majorBaseUrl.put("기술교육과", "https://techedu.cnu.ac.kr");
+
+        majorBaseUrl.put("화학공학교육과", "https://techedu.cnu.ac.kr");
+
+        majorBaseUrl.put("불어불문학과", "https://french.cnu.ac.kr");
+
+        majorBaseUrl.put("교육학과", "https://education.cnu.ac.kr");
+
+        majorBaseUrl.put("정치외교학과", "https://politics.cnu.ac.kr");
+
+        majorBaseUrl.put("국사학과", "https://koreanhistory.cnu.ac.kr");
+
+        majorBaseUrl.put("사회복지학과", "https://welfare.cnu.ac.kr");
+
+        majorBaseUrl.put("음악과", "https://music.cnu.ac.kr");
+
+        majorBaseUrl.put("사회과학대학", "https://socialscience.cnu.ac.kr");
+
+        majorBaseUrl.put("간호대학", "https://nursing.cnu.ac.kr");
+
+        majorBaseUrl.put("문헌정보학과", "https://lis.cnu.ac.kr");
+
+        majorBaseUrl.put("생활과학대학", "https://homeco.cnu.ac.kr");
+
+        majorBaseUrl.put("생화학과", "https://biochemistry.cnu.ac.kr");
+
+        majorBaseUrl.put("정보통계학과", "https://stat.cnu.ac.kr");
+
+        majorBaseUrl.put("수학교육과", "https://mathedu.cnu.ac.kr");
+
+        majorBaseUrl.put("언론정보학과", "https://comm.cnu.ac.kr");
+
+        majorBaseUrl.put("수학과", "https://math.cnu.ac.kr");
+
+        majorBaseUrl.put("사회학과", "https://socio.cnu.ac.kr");
+
+        majorBaseUrl.put("고고학과", "https://cnuarchaeology.cnu.ac.kr");
+
+        majorBaseUrl.put("사학과", "https://history.cnu.ac.kr");
+
+        majorBaseUrl.put("중어중문학과", "https://cnucn.cnu.ac.kr");
+
+        majorBaseUrl.put("철학과", "https://philosophy.cnu.ac.kr");
+
+        majorBaseUrl.put("전기공학과", "https://elec.cnu.ac.kr");
+
+        majorBaseUrl.put("도시·자치융합학과", "https://autonomy.cnu.ac.kr");
+
+        majorBaseUrl.put("영어교육과", "https://enedu.cnu.ac.kr");
+
+        majorBaseUrl.put("무역학과", "https://trade.cnu.ac.kr");
+
+        majorBaseUrl.put("경제학과", "https://economics.cnu.ac.kr");
+
+        majorBaseUrl.put("화학과", "https://chem.cnu.ac.kr");
+
+        majorBaseUrl.put("독어독문학과", "https://german.cnu.ac.kr");
+
+        majorBaseUrl.put("토목공학과", "https://civileng.cnu.ac.kr");
+
+        majorBaseUrl.put("국어교육과", "https://koreanedu.cnu.ac.kr");
+
+        majorBaseUrl.put("자연과학대학", "https://cns.cnu.ac.kr");
+
+        majorBaseUrl.put("의류학과", "https://cloth.cnu.ac.kr");
+
+        majorBaseUrl.put("소비자학과", "https://cli.cnu.ac.kr");
+
+        majorBaseUrl.put("해양환경과학과", "https://ocean.cnu.ac.kr");
+
+        majorBaseUrl.put("식품영양학과", "https://fdnutri.cnu.ac.kr");
+
+        majorBaseUrl.put("천문우주과학과", "https://astro1.cnu.ac.kr");
+
+        majorBaseUrl.put("한문학과", "https://sinokor.cnu.ac.kr");
+
+        majorBaseUrl.put("메카트로닉스공학과", "https://mec.cnu.ac.kr");
+
+        majorBaseUrl.put("스포츠과학과", "https://sports.cnu.ac.kr");
+
+        majorBaseUrl.put("아시아비즈니스국제학과", "https://abis.cnu.ac.kr");
+
+        majorBaseUrl.put("경상대학", "https://cem.cnu.ac.kr");
+
+        majorBaseUrl.put("물리학과", "https://physics.cnu.ac.kr");
+
+        majorBaseUrl.put("의과대학", "https://medicine.cnu.ac.kr");
+
+        majorBaseUrl.put("언어학과", "https://cnulinguistics.cnu.ac.kr");
+
+        majorBaseUrl.put("언어병리학과", "https://cnuslp.cnu.ac.kr");
+
+        majorBaseUrl.put("영어영문학과", "https://english.cnu.ac.kr");
+
+        majorBaseUrl.put("조소과", "https://sculpture.cnu.ac.kr");
+
+        majorBaseUrl.put("생명환경화학과", "https://bec.cnu.ac.kr");
+
+        majorBaseUrl.put("인문대학", "https://human.cnu.ac.kr");
+
+        majorBaseUrl.put("자유전공학부", "https://liberalarts.cnu.ac.kr");
+
+        majorBaseUrl.put("생명시스템과학대학", "https://cbb.cnu.ac.kr");
+
+        majorBaseUrl.put("생물과학과", "https://cbb.cnu.ac.kr");
+
+        majorBaseUrl.put("미생물분자생명과학과", "https://cbb.cnu.ac.kr");
+
+        majorBaseUrl.put("신소재공학과", "https://mse.cnu.ac.kr");
+
+        majorBaseUrl.put("무용학과", "https://dance.cnu.ac.kr");
+
+        majorBaseUrl.put("응용화학공학과", "https://ceac.cnu.ac.kr");
+
+        majorBaseUrl.put("기계공학부", "https://me.cnu.ac.kr");
+
+        majorBaseUrl.put("농업생명과학대학", "https://cals.cnu.ac.kr");
+
+        majorBaseUrl.put("식물자원학과", "https://crop.cnu.ac.kr");
+
+        majorBaseUrl.put("원예학과", "https://horti.cnu.ac.kr");
+
+        majorBaseUrl.put("식품공학과", "https://fst.cnu.ac.kr");
+
+        majorBaseUrl.put("농업경제학과", "https://agrieco.cnu.ac.kr");
+
+        majorBaseUrl.put("응용생물학과", "https://abio.cnu.ac.kr");
+
+        majorBaseUrl.put("지역환경토목학과", "https://are.cnu.ac.kr");
+
+        majorBaseUrl.put("인재개발원", "https://job.cnu.ac.kr");
+
+        majorBaseUrl.put("산림환경자원학과", "https://forestry.cnu.ac.kr");
+
+        majorBaseUrl.put("행정학부", "https://cnupa.cnu.ac.kr");
+
+        majorBaseUrl.put("약학대학", "https://pharm.cnu.ac.kr");
+
+        majorBaseUrl.put("경영학부", "https://biz.cnu.ac.kr");
+
+        majorBaseUrl.put("우주지질학과", "https://biz.cnu.ac.kr");
+
+        majorBaseUrl.put("바이오AI융합학과", "https://bioai.cnu.ac.kr");
+
+        majorBaseUrl.put("자율운항시스템공학과", "https://avse.cnu.ac.kr");
+
+        majorBaseUrl.put("스마트시티건축공학과", "https://smartarchi-eng.cnu.ac.kr");
+
+        majorBaseUrl.put("인공지능학과", "https://ai.cnu.ac.kr");
+
+        majorBaseUrl.put("생명정보융합학과", "https://dcbi.cnu.ac.kr");
+
+        majorBaseUrl.put("소프트웨어중심사업단", "https://swuniv.cnu.ac.kr");
+
+        majorBaseUrl.put("공과대학", "https://eng.cnu.ac.kr");
+
+        majorBaseUrl.put("전파정보통신공학부", "https://rice.cnu.ac.kr");
+
+        majorBaseUrl.put("환경공학과", "https://envi.cnu.ac.kr");
+
+        majorBaseUrl.put("국제학부", "https://cnusis.cnu.ac.kr");
+
+        majorBaseUrl.put("전자공학과", "https://ee.cnu.ac.kr");
+
+        majorBaseUrl.put("기술실용화융합학과", "https://star.cnu.ac.kr");
+
+
+    }
+}
