97e555ca534e6da508ead199d6ca3e50afc7cc47
EunKyung Lee <2dmsrud2002@naver.com>
2023-05-23 22:15:00 +0900

    lombok

--- src/main/java/com/cnu/contestarchive/Domain/Favorite.java
+++ src/main/java/com/cnu/contestarchive/Domain/Favorite.java
@@ -1,9 +1,16 @@
 package com.cnu.contestarchive.Domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
 @Entity  // 데이터베이스의 테이블과 매핑
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
 public class Favorite {
 
     @Id  // 테이블의 기본 키(primary key)
@@ -12,36 +19,4 @@ public class Favorite {
     private String section;
 
     private String title;
-
-    public Favorite() {
-
-    }
-
-    public Favorite(String id, String section, String title) {
-        this.Id = id;
-        this.section = section;
-        this.title = title;
-    }
-    public String getId() {
-        return Id;
-    }
-    public String getSection() {
-        return section;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setId(String Id) {
-        this.Id = Id;
-    }
-
-    public void setSection(String section) {
-        this.section = section;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
 }
